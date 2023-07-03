7a9c83f76a58b64d30e2579ec5e436b80e90b157
GlennGerada <glenngerada8483@gmail.com>
2023-07-03 17:25:30 +0800

    Removed word

--- project/footer.php
+++ project/footer.php
@@ -36,6 +36,6 @@
 
    </div>
 
-   <p class="credit"> &copy; @ <?php echo date('Y'); ?> by <span>Glenn Gerada</span> </p>
+   <p class="credit"> &copy; <?php echo date('Y'); ?> by <span>Glenn Gerada</span> </p>
 
 </section>
